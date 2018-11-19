38e07597e7732543a4a28acb2009c06fc8e626c0
capable-publisher-7225 <marcusplenty@gmail.com>
2018-11-19 01:06:46 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -177,6 +177,20 @@ def team_names
 end
 
 
+def player_numbers(team_name)
+  hashy = game_hash
+  hashy.each do |k1,v1|
+    v1.each do |k2,v2|
+      if k2 == :players
+        v2.each do |k3,v3|
+          if k3 == player_name
+            return hashy[k1][k2][k3][:points]
+          end
+        end
+      end
+    end
+  end
+end
 
 
 
